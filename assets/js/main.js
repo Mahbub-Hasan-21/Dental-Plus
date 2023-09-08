21751060cd688b7d0ce71b73dd19b79838bcb902
Mahbub-Hasan-21 <141586857+Mahbub-Hasan-21@users.noreply.github.com>
2023-09-08 07:12:57 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+$('.banner').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    smartSpeed:700,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+});
+
+$(document).ready(function(){
+    $('.client').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    smartSpeed:700,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+});
